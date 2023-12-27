f109b050ab53cd3cd07c082394b03aca6441a25d
DNAMcKnight <conner.cmk9040@gmail.com>
2023-12-27 20:30:37 +0600

    update v1.1

--- app/admin.py
+++ app/admin.py
@@ -4,8 +4,12 @@ from .models import MCQ, Import,UserQuestions,UserInfo
 # admin.site.register(MCQ)
 class questions(admin.ModelAdmin):
     list_display = ['question', 'options', 'answer',]
+class user_questions(admin.ModelAdmin):
+    list_display = ['user','encoded_id', 'used_question' ]
+class user_info(admin.ModelAdmin):
+    list_display = ['user','correct','incorrect','phone']
 # Register your models here.
 admin.site.register(MCQ, questions)
 admin.site.register(Import)
-admin.site.register(UserQuestions)
-admin.site.register(UserInfo)
+admin.site.register(UserQuestions, user_questions)
+admin.site.register(UserInfo, user_info)
